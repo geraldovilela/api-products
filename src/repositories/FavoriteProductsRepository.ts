54459330c7bde05cf8945684ffd166cd7db98b84
Geraldo <geraldovilela19@gmail.com>
2021-06-27 09:41:10 -0400

    dev: remove of unused repositories folder;

--- src/repositories/FavoriteProductsRepository.ts
+++
@@ -1,9 +0,0 @@
-/* import { EntityRepository, Repository } from "typeorm";
-import { FavoretiProducts } from "../entities/FavoriteProducts";
-
-@EntityRepository(FavoretiProducts)
-class FavoriteProductsRepository extends Repository<FavoretiProducts> {
-
-}
-
-export { FavoriteProductsRepository } */
--- src/repositories/ProductsRepository.ts
+++
@@ -1,9 +0,0 @@
-/* import { EntityRepository, Repository } from "typeorm";
-import { Product } from "../entities/Product";
-
-@EntityRepository(Product)
-class ProductsRepositorie extends Repository<Product> {
-
-}
-
-export { ProductsRepositorie }; */

47b82a9a14e4c8b5261d32f6138310af79b5b474
Geraldo <geraldovilela19@gmail.com>
2021-06-27 05:00:16 -0400

    dev: swaping typeorm for mongoose for db connection;

---
+++ src/config/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+  mongo_url:"mongodb+srv://<@usernameB>:<@password@>@testeproducts.hofnt.mongodb.net/<@@Nome do DB@@>?retryWrites=true&w=majority",
+  mongo_options:{
+    "useNewUrlParser": true,
+    "useUnifiedTopology": true,
+    "useCreateIndex":true,
+  }
+}

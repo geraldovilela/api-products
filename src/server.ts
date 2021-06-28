f37141883acd67534df21748eb7b0ee5f880d847
Geraldo <geraldovilela19@gmail.com>
2021-06-27 21:43:45 -0400

    dev: cors;

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
+import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
 
 import { router } from './router';
 
 const app = express();
-
+app.use(cors())
 app.use(express.json());
 app.use(router);
 
